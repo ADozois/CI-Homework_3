03589494072d4143ecf237df612bfb66af30ba6f
Antoine Dozois <dozois.a@gmail.com>
2017-01-02 13:06:53 +0100

    Initial cmake-build

---
+++ cmake-build-debug/CMakeFiles/CI_Homework_3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CI_Homework_3.dir/Problem_1/Problem_1.c.o"
+  "CI_Homework_3.pdb"
+  "CI_Homework_3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CI_Homework_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
